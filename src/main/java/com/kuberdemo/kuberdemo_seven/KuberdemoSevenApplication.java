952c8e429c3ad6847307352af39c8f276fff4264
priyanshikotian0902 <priyanshi.kotian@mitoae.ac.in>
2024-06-05 17:45:50 +0530

    Initial commit

---
+++ src/main/java/com/kuberdemo/kuberdemo_seven/KuberdemoSevenApplication.java
@@ -0,0 +1,13 @@
+package com.kuberdemo.kuberdemo_seven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberdemoSevenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberdemoSevenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuberdemo/kuberdemo_seven/KuberdemoSevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuberdemo.kuberdemo_seven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberdemoSevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
